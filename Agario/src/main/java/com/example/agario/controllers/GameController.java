749a0f1e44284712aa1beeb51b26e2f8caab28f1
Cassandre9610 <cassandre.yontailleux@gmail.com>
2025-03-25 11:08:46 +0100

    fx:id added

--- Agario/src/main/java/com/example/agario/controllers/GameController.java
+++ Agario/src/main/java/com/example/agario/controllers/GameController.java
@@ -1,4 +1,16 @@
 package com.example.agario.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
 public class GameController {
+    @FXML private TextField TchatTextField;
+    @FXML private Pane GamePane;
+    @FXML private ListView LeaderBoardListView;
+    @FXML private ListView ChatListView;
+
 }
