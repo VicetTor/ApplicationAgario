8426b619cedfcb74d8bdede6343870cbef09e0b3
Victor <victoryvray@gmail.com>
2025-03-26 19:48:47 +0100

    Fixing pellets mass

--- Agario/src/main/java/com/example/agario/models/Pellet.java
+++ Agario/src/main/java/com/example/agario/models/Pellet.java
@@ -1,7 +1,9 @@
 package com.example.agario.models;
 
+import java.util.Random;
+
 public class Pellet extends Entity{
     public Pellet(double x, double y) {
-        super(x, y,1);
+        super(x, y, new Random().nextInt(1, 11) / 10.0);
     }
 }
